993304f02678e85a9c8269c36af6f6e9322e3c63
Chaima Bouhlel <chaima.bouhlel@insat.u-carthage.tn>
2023-05-29 15:53:52 +0100

    add news generation

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,6 @@
 import {useState} from "react";
 import axios from "axios";
 import {useMutation} from "@tanstack/react-query";
-import { log } from "console";
-
-
 
 export default function Home() {
     const [data, setData] = useState(null);
@@ -11,11 +8,11 @@ export default function Home() {
 
     const mutation = useMutation({
         onSuccess: (data) => {
-            setData(data.poem)
+            setData(data.poem || data.news)
         },
-        mutationFn: (input) => {
+        mutationFn: ({textType,input}) => {
             console.log(input)
-            return axios.post("http://localhost:5000/generate_poem", input).then(response => response.data)
+            return axios.post(`http://localhost:5000/generate_${textType}`, {input_text: input}).then(response => response.data)
         }
     })
 
@@ -33,20 +30,20 @@ export default function Home() {
              </div>
              <div className="flex flex-row shadow-2xl">
                  <textarea rows="1" value={inputText} className="caret-pink-600 rounded-bl-2xl rounded-tl-2xl resize-y w-full p-2" onChange={handleChange}/>
-                 <button className="bg-white text-sm p-1 ml-0.5 font-bold" onClick={()=>{mutation.mutate({input_text:inputText})}}>{"Generate Poem"}</button>
-                 <button className="bg-white rounded-tr-2xl rounded-br-2xl text-sm ml-0.5 p-1 font-bold">{"Generate News"}</button>
+                 <button className="bg-white text-sm p-1 ml-0.5 font-bold" onClick={()=>{mutation.mutate({textType:"poem", input:inputText})}}>{"Generate Poem"}</button>
+                 <button className="bg-white rounded-tr-2xl rounded-br-2xl text-sm ml-0.5 p-1 font-bold" onClick={()=>{mutation.mutate({textType:"news", input:inputText})}}>{"Generate News"}</button>
              </div>
              <div className="mt-6 text-left bg-black/30 backdrop-opacity-10 rounded-2xl p-4">
                  <h6 className="text-lg font-bold">Generated text:</h6>
-                 <p>
+                 <pre>
 
-                     { mutation.isLoading? "poem loading..." :
+                     { mutation.isLoading? "loading..." :
                          (
-                         ! data ? "No poem yet"
+                         ! data ? "No poem or news yet"
                             :
                                  data
                          )}
-                 </p>
+                 </pre>
              </div>
          </div>
       </>
