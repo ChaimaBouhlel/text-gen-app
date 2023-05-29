1a29d2c38b081ec7260b22a0484a23c556ddf5f4
Chaima Bouhlel <chaima.bouhlel@insat.u-carthage.tn>
2023-05-29 14:29:23 +0100

    add post request

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,25 +1,25 @@
-const text ="The leaves are falling down so fast\n" +
-    "Autumn winds so cold, it can't last\n" +
-    "\n" +
-    "The trees are changing colors bright\n" +
-    "Red, orange, yellow, what a sight\n" +
-    "\n" +
-    "Pumpkins sitting in the fields so round\n" +
-    "Cornstalks standing up high, abound\n" +
-    "\n" +
-    "The smell of apple cider in the air\n" +
-    "Halloween costumes, people prepare\n" +
-    "\n" +
-    "Thanksgiving turkey, stuffing and pie\n" +
-    "Family gatherings, don't be shy\n" +
-    "\n" +
-    "As autumn comes, it's time for change\n" +
-    "Embrace it all, it's quite strange\n" +
-    "\n" +
-    "The chill in the air, the crunch underfoot\n" +
-    "The autumn season, full of soot."
+import {useState} from "react";
+import axios from "axios";
+import {useMutation} from "@tanstack/react-query";
+
+
+
 export default function Home() {
-  return (
+    const [data, setData] = useState(null);
+    const [inputText, setInputText] = useState("")
+
+    const mutation = useMutation({
+        onSuccess: (data) => {
+            setData(data)
+        },
+        mutationFn: (input) => {
+            console.log(input)
+            return axios.post("http://localhost:5000/generate_poem", input).then(res => res.data.json())
+        }
+    })
+
+    const handleChange = (e) => {setInputText(e.target.value)}
+    return (
       <>
          <div className="bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 text-center pt-32 w-screen h-screen px-[15%] lg:px-[25%]">
              <div className="mb-12">
@@ -31,14 +31,20 @@ export default function Home() {
                  </p>
              </div>
              <div className="flex flex-row shadow-2xl">
-                 <textarea rows="1" className="caret-pink-600 rounded-bl-2xl rounded-tl-2xl resize-y w-full p-2"/>
-                 <button className="bg-white text-sm p-1 ml-0.5 font-bold">{"Generate Poem"}</button>
+                 <textarea rows="1" value={inputText} className="caret-pink-600 rounded-bl-2xl rounded-tl-2xl resize-y w-full p-2" onChange={handleChange}/>
+                 <button className="bg-white text-sm p-1 ml-0.5 font-bold" onClick={()=>{mutation.mutate({input_text:inputText})}}>{"Generate Poem"}</button>
                  <button className="bg-white rounded-tr-2xl rounded-br-2xl text-sm ml-0.5 p-1 font-bold">{"Generate News"}</button>
              </div>
              <div className="mt-6 text-left bg-black/30 backdrop-opacity-10 rounded-2xl p-4">
                  <h6 className="text-lg font-bold">Generated text:</h6>
                  <p>
-                     {text}
+
+                     { mutation.isLoading? "poem loading..." :
+                         (
+                         ! data ? "No poem yet"
+                            :
+                                 data
+                         )}
                  </p>
              </div>
          </div>
