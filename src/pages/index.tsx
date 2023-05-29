5f39cb7025978da92abe2c5c4f37b2bf547a1a4b
Chaima Bouhlel <chaima.bouhlel@insat.u-carthage.tn>
2023-05-29 16:11:27 +0100

    fix styles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -19,7 +19,7 @@ export default function Home() {
     const handleChange = (e) => {setInputText(e.target.value)}
     return (
       <>
-         <div className="bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 text-center pt-32 w-screen h-screen px-[15%] lg:px-[25%]">
+         <div className="text-center pt-32 px-[15%] lg:px-[25%]">
              <div className="mb-12">
                  <h1 className="text-3xl font-bold text-white text-6xl mb-6">
                      The modern Shakespeare
@@ -30,10 +30,10 @@ export default function Home() {
              </div>
              <div className="flex flex-row shadow-2xl">
                  <textarea rows="1" value={inputText} className="caret-pink-600 rounded-bl-2xl rounded-tl-2xl resize-y w-full p-2" onChange={handleChange}/>
-                 <button className="bg-white text-sm p-1 ml-0.5 font-bold" onClick={()=>{mutation.mutate({textType:"poem", input:inputText})}}>{"Generate Poem"}</button>
-                 <button className="bg-white rounded-tr-2xl rounded-br-2xl text-sm ml-0.5 p-1 font-bold" onClick={()=>{mutation.mutate({textType:"news", input:inputText})}}>{"Generate News"}</button>
+                 <button className="bg-purple-50 text-indigo-500 text-sm p-1 ml-0.5 font-bold" onClick={()=>{mutation.mutate({textType:"poem", input:inputText})}}>{"Generate Poem"}</button>
+                 <button className="bg-pink-50 text-pink-500 rounded-tr-2xl rounded-br-2xl text-sm ml-0.5 p-1 font-bold" onClick={()=>{mutation.mutate({textType:"news", input:inputText})}}>{"Generate News"}</button>
              </div>
-             <div className="mt-6 text-left bg-black/30 backdrop-opacity-10 rounded-2xl p-4">
+             <div className="mt-6 text-left bg-black/30 backdrop-opacity-10 rounded-2xl p-4 mb-20">
                  <h6 className="text-lg font-bold">Generated text:</h6>
                  <pre>
 
